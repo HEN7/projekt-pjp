f92ee561b8af691cf920860647f7f78e5cb9fc8b
HEN7 <m.rachmatul@gmail.com>
2015-12-03 09:40:00 +0100

    piąty commit
    poprawiono kilka bitmap

--- dung_exp.cpp
+++ dung_exp.cpp
@@ -1,5 +1,5 @@
 #include <allegro5/allegro.h>
-#include<allegro5/allegro_primitives.h>
+#include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
